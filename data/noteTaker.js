31f30619f73bc5b01c1e7c9107235be020342d4a
Denise <dmtuominen@gmail.com>
2021-09-18 12:36:00 -0600

    starter files

---
+++ data/noteTaker.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { noteTaker } = require('./data/noteTaker');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+
+
+app.get('/api/' (req, res) => {
+    res.send('Hello!');
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
